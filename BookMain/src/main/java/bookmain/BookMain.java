0d882a421bc13b734d06d53fae0b3c3010e5b710
Kurt Kreilinger <k.kreilinger@gmail.com>
2025-06-29 08:13:10 +0200

    Update BookMain.java

--- BookMain/src/main/java/bookmain/BookMain.java
+++ BookMain/src/main/java/bookmain/BookMain.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package bookmain;
 
-/**
- *
- * @author kkrei
- */
 public class BookMain {
 
     public static void main(String[] args) {
